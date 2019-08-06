ed37fb737c317b664791c41445d4f41ab168cf72
xcal1bur <david.voigt1998@gmail.com>
2019-08-07 00:26:41 +0200

    Various changes and fixes

--- giter.py
+++ giter.py
@@ -1,4 +1,4 @@
-import os, subprocess, colorama, argparse, sys
+import os, subprocess, colorama, argparse, sys, giter
 from github import Github
 
 def get_credentials():
@@ -9,16 +9,19 @@ def get_credentials():
     username = str()
     passwd = str()
     
-    #Set Github username
+    # Set Github username
     print(colorama.Fore.YELLOW+"Enter Github username: ", end=" ")
     username = input(colorama.Fore.RESET)
-    #Set Github password associated with the given username
+    # Set Github password associated with the given username
     print(colorama.Fore.YELLOW+"Enter Github password: ", end=" ")
     passwd = input(colorama.Fore.RESET)
 
     return username, passwd
 
 def authenticat_user():
+    """ 
+    Authenticate the credentials provided and return a Github user object.
+    """
     username, passwd = get_credentials()
 
     try:
@@ -30,10 +33,12 @@ def authenticat_user():
         print(colorama.Fore.RED+repr(e))
 
 def create_repo():
+    """
+    Creates a remote repository on Github.com and returns username and repo name.
+    """
     repo = str()
     user = authenticat_user()
-    user
-    #Set repo name
+    # Set repo name
     print(colorama.Fore.YELLOW+"\nEnter repository name: ", end=" ")
     repo = input(colorama.Fore.RESET)
 
@@ -41,38 +46,45 @@ def create_repo():
         print(colorama.Fore.YELLOW+f"\nCreating repo {repo}")
         user.create_repo(repo)
         print(colorama.Fore.GREEN+f"Repository {user.get_repo(repo).full_name} has been created")
-        #Return username and repo name
+        # Return username and repo name
         return user.name, repo
     except Exception as e:
         print(colorama.Fore.RED, repr(e))
 
-def git_init(username, repo_name, https=True):
+def git_init(username, repo_name, https=False):
     """
-    Initializes a git repo at the path 
+    Initializes a git repo where this application has been called from!!! 
     """
+    print()
     subprocess.run("git init", shell=True)
     subprocess.run("git add *", shell=True)
     subprocess.run("git commit -m \"Initial commit\"", shell=True)
     if https:
-        subprocess.run(f"git add origin https://github.com/{username}/{repo_name}", shell=True)
+        subprocess.run(f"git remote add origin https://github.com/{username}/{repo_name}", shell=True)
     else:
-        subprocess.run(f"git add origin git@github.com:{username}/{repo_name}", shell=True)
+        subprocess.run(f"git remote add origin git@github.com:{username}/{repo_name}.git", shell=True)
     subprocess.run("git push -u origin master", shell=True)
 
 
 if __name__ == "__main__":
     colorama.init()
-    parser = argparse.ArgumentParser(description="Command line application to quickly set up new repositories on Github and add to as origin to your local repo.")
+    parser = argparse.ArgumentParser(description="Command line application to quickly set up a new remote repository, initialize a local git repository and add the remote repo.")
     parser.add_argument("--init", "-i", action="store_true")
     parser.add_argument("--https", action="store_true")
     parser.add_argument("--create", "-c", action="store_true")
+    parser.add_argument("--doc", action="store_true")
     args = parser.parse_args()
 
+    # Create a new repo on Github.com
     if args.create:
         username, repo_name = create_repo()
+    # If a new repo on Github.com already has been created also initialize a local git repo
     if args.init and args.create:
         git_init(username, repo_name, args.https)
+    # Only initialize a local git repo. User will be prompted to enter his Github.com username and the desired repo name (has to be created already). 
     elif args.init and args.create == False:
-        username = input(colorama.Fore.YELLOW+"Enter username: ")
-        repo_name = input(colorama.Fore.YELLOW+"Enter repository name: ")
+        username = input(colorama.Fore.YELLOW+"Enter username: "+colorama.Fore.RESET)
+        repo_name = input(colorama.Fore.YELLOW+"Enter repository name: "+colorama.Fore.RESET)
         git_init(username, repo_name, args.https)
+    if args.doc:
+        help(giter)
