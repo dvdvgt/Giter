075b68ac50e1bb313c7110df59021add64a50fdf
Xcal1bur <david.voigt1998@gmail.com>
2019-08-14 10:35:48 +0200

    Created new python file for colors

--- src/giter.py
+++ src/giter.py
@@ -3,20 +3,9 @@
     Command line application to quickly set up a new remote repository, initialize a local git repository and add the remote repo.
 """
 import os, subprocess, argparse, sys, getpass, giter, requests, time
+from colors import color
 from github import Github
 from bs4 import BeautifulSoup
-
-class color:
-    PURPLE = '\033[95m'
-    CYAN = '\033[96m'
-    DARKCYAN = '\033[36m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    END = '\033[0m'
 
 def authenticat_user(username, passwd):
     """ 
