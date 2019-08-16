a8c9fe34f0c8800236a5d3da01dc15b0573fc2b3
Xcal1bur <david.voigt1998@gmail.com>
2019-08-16 19:30:14 +0200

    Added version

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup, find_packages
+import os
 
-with open("README.md", "r") as file:
+with open(os.path.join(os.path.realpath(__file__)), "r") as file:
     long_description = file.read()
 
 setup(
     name='giter',
+    version='1.0.0',
     packages=find_packages(),
     author='David Voigt',
     description='Command line application to quickly set up new repositories on Github and add it as origin to your local repo.',
     long_description=long_description,
     url='https://github.com/Xcal1bur/Giter',
     license='GNU General Public License v3.0',
-    install_requires=['requests', 'bs4']
+    install_requires=['requests', 'beautifulsoup4']
 )
